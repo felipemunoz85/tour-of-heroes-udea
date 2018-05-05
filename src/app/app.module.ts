9e53158746ae4e84eb404a064dc5d591687fa6a5
Felipe Muñoz <luis.f.munoz1985@gmail.com>
2018-05-05 11:09:22 -0500

    ajuste para consumir servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HttpClientModule }    from '@angular/common/http';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
@@ -31,12 +29,6 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
   ],
   providers: [
     HeroService,
